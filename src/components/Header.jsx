e7655bfc017ee94f35304e9273c9afe42981d611
Pms-crypto <poorvashukla315@gmail.com>
2024-03-09 15:39:40 +0530

    update add button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,20 @@
 import PropTypes from 'prop-types'
 import Button from './Button.jsx';
 
-const Header = ({title}) => {
-  const onClick = () => {
-
-  }
+const Header = ({title, onAdd, showAddTask}) => {
   
   return (
     <header className="header">
       {title}
-      <Button color='green' text='Add' onClick={onClick}/>
+      <Button color={showAddTask ? 'red' : 'green'} text={showAddTask? 'Close' : 'Add'} onClick={onAdd}/>
     </header>
   )
 }
 
 Header.propTypes = {
   title: PropTypes.string,
+  onAdd: PropTypes.func,
+  showAddTask: PropTypes.bool,
 }
 
 export default Header
