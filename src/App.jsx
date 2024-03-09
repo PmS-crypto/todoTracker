5a57bd4bba094f53c8e931854900065fc791388f
Pms-crypto <poorvashukla315@gmail.com>
2024-03-09 14:47:49 +0530

    feat: add form to enter tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Header from "./components/Header.jsx";
 import Tasks from "./components/Tasks.jsx";
 import { useState } from "react";
-
+import AddTask from "./components/AddTask.jsx";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
         id:1,
@@ -19,6 +20,12 @@ function App() {
     }
 ]);
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random()*10000)+1;
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask]);
+  }
+
   const deleteTask = (id) => {
     setTasks(tasks.filter(task => task.id !== id))
   }
@@ -30,6 +37,7 @@ function App() {
   return (
     <div className="container">
       <Header title="Task Tracker"/>
+      <AddTask onAdd={addTask}/>
       {tasks.length>0?<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>:"No Tasks to show"}
     </div>
   )
